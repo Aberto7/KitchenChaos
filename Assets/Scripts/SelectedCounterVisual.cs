346c5a4575bff8892f2d8dd9d0a3f5c1d0c72783
Abdul Baseer Phulpoto <117812192+Aberto7@users.noreply.github.com>
2025-03-23 10:45:47 +0500

    Added Ingredients as scriptable objects
    Added tomato and cheese-block as ingredients in the form of scriptable objects which is helpful for further expanding (adding) new ingredient (items) in the future.

--- Assets/Scripts/SelectedCounterVisual.cs
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -9,13 +9,28 @@ public class SelectedCounterVisual : MonoBehaviour
     // That is Why I am using Sinleton patern here.
 
 
+    [SerializeField] private ClearCounter clearCounter;
+    [SerializeField] private GameObject visualGameObject;
+
     private void Start()
     {
         Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
     }
 
     private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e){
-        throw new System.NotImplementedException();
+        if (e.selectedCounter == clearCounter){
+            Show();
+        } else {
+            Hide();
+        }
+    }
+
+    private void Show(){
+        visualGameObject.SetActive(true);
+    }
+
+    private void Hide(){
+        visualGameObject.SetActive(false);
     }
 }
 

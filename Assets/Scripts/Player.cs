27005caa2ecdc9042a176ca32ec47353278a60f0
Abdul Baseer Phulpoto <117812192+Aberto7@users.noreply.github.com>
2025-03-30 23:22:57 +0500

    Added SoundEffects
    And changed a whole-lot-of Scripts. (Too Tired rn)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,15 +10,11 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour, IKitchenObjectParent
 {
 
-
-
     public static Player Instance { get; private set; }
 
 
 
-
-
-
+    public event EventHandler OnPickedSomething;
     public event EventHandler <OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs {
         public BaseCounter selectedCounter;
@@ -198,6 +194,10 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     public void SetKitchenObject(KitchenObject kitchenObject){
         this.kitchenObject = kitchenObject;
+
+        if (kitchenObject != null){
+            OnPickedSomething?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public KitchenObject GetKitchenObject(){

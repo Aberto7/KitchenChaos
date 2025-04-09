46c45365957ddb6e38ba328c7b8b9b76667779b0
Abdul Baseer Phulpoto <117812192+Aberto7@users.noreply.github.com>
2025-04-09 22:49:41 +0500

    Polish (Added a Delivery Result Canvas)
    Added a DeliveryResult canvas, and edited the time from 20f to 35f.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour {
     private State state;
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 20f;
+    private float gamePlayingTimerMax = 35f;
     private bool IsGamePaused = false;
     
     
